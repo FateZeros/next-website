d4873b12f126e40e36a685b812715bcf6cf08041
yejf <490293266@qq.com>
2020-02-12 18:45:46 +0800

    update

--- front-web/components/footer/footer.js
+++ front-web/components/footer/footer.js
@@ -1,11 +1,56 @@
+import React, { Component } from 'react'
+
 import styles from './footer.less'
 
-const Footer = () => (
-  <div className={styles.footer}>
-    <div className={styles.copyRight}>
-      <p>CopyRight©2019</p>
-    </div>
-  </div>
-)
+const footerImgs = [
+  {
+    id: 1,
+    name: 'footer1-54.png',
+    info: '0755-8358-3580'
+  },
+  {
+    id: 2,
+    name: 'footer2-54.png',
+    info: '高安全保障'
+  },
+  {
+    id: 3,
+    name: 'footer3-54.png',
+    info: '高效的团队'
+  }
+].map(img => {
+  img.url = `/static/images/${img.name}`
+  return img
+})
+
+class Footer extends Component {
+  renderFooterImg() {
+    return (
+      <ul className={styles.footerImgs}>
+        {footerImgs.map(item => {
+          return (
+            <li key={item.id} className={styles.footerImg}>
+              <img src={item.url} alt="" className={styles.footerItemImg} />
+              {item.info}
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div className={styles.footer}>
+        <div className={styles.footerContainer}>
+          {this.renderFooterImg()}
+          <div className={styles.copyRight}>
+            <p>CopyRight©2019</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Footer
--- front-web/pages/home/home.news.js
+++ front-web/pages/home/home.news.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
-import { Tabs } from 'antd'
+import { Tabs, Row, Icon } from 'antd'
 
 const { TabPane } = Tabs
 
 import styles from './home.less'
+
+const news1 = [
+  {
+    id: 1,
+    news: '香港公司注册，香港公司包开户'
+  },
+  {
+    id: 2,
+    news: '注册香港公司的好处'
+  },
+  {
+    id: 3,
+    news: '内地人注册香港公司的好处'
+  },
+  {
+    id: 4,
+    news: '注册一家香港公司最适合做什么业务'
+  },
+  {
+    id: 5,
+    news: '完成注册香港离岸公司可以在大陆经营吗'
+  },
+  {
+    id: 6,
+    news: '什么企业适合注册香港公司'
+  }
+]
 
 class HomeNews extends Component {
   render() {
@@ -13,7 +40,19 @@ class HomeNews extends Component {
           <div className={styles.newsTitle}>新闻中心</div>
           <Tabs defaultActiveKey="1">
             <TabPane tab="注册香港公司" key="1">
-              1111
+              {news1.map(item => {
+                return (
+                  <Row className={styles.newsRow} key={item.id}>
+                    <span>
+                      <Icon
+                        type="caret-right"
+                        className={styles.newsRowLeftIcon}
+                      />
+                      {item.news}
+                    </span>
+                  </Row>
+                )
+              })}
             </TabPane>
             <TabPane tab="香港公司年审" key="2">
               1111
